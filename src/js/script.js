fc431535bf10220ae1c2d9066dfaa97f1ab6f8c0
bjornwildenstam <bjorn.wildenstam@yh.nackademin.se>
2026-01-26 00:38:11 +0100

    changed url again

--- src/js/script.js
+++ src/js/script.js
@@ -26,9 +26,7 @@ function injectStyles() {
   if (document.querySelector('link[href*="style.css"]')) return;
   const link = document.createElement('link');
   link.rel = 'stylesheet';
-  link.href = isLocalhost
-    ? 'src/css/style.css'
-    : 'https://seos-cookie-banner.vercel.app/src/css/style.css';
+  link.href = isLocalhost ? 'css/style.css' : 'https://seos-cookie-banner.vercel.app/css/style.css';
   document.head.appendChild(link);
 }
 
